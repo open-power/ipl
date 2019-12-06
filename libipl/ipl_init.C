48e49396665d8df77928376d909ca5a555e5cb76
Alistair Popple <alistair@popple.id.au>
2019-12-06 15:07:27 +1100

    ipl_init: Use device tree selected by libpdbg
    Signed-off-by: Alistair Popple <alistair@popple.id.au>

--- libipl/ipl_init.C
+++ libipl/ipl_init.C
@@ -9,19 +9,7 @@ extern "C" {
 #include "config.h"
 #include "libekb.H"
 
-static struct mmap_file_context *mfile;
-
 int ipl_init(void)
 {
-    mfile = mmap_file_open(DTB_FILE, true);
-
-	if (!mfile)
-		return -1;
-
-	return libekb_init(mmap_file_ptr(mfile));
-}
-
-void ipl_close(void)
-{
-	mmap_file_close(mfile);
+	return libekb_init();
 }
