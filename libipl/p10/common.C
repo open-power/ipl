04ca607eded4adb7458d29660798cd8f29ab7f9f
Jayanth Othayoth <ojayanth@in.ibm.com>
2020-01-23 17:39:43 -0600

    ipl: Hostboot istep timeout update
    Updated Hostboot istep timeout to 30 minutes from
    default 2.5 sec. This is based on the recomentation
    from hostboot.
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Reviewed-by: Amitay Isaacs <amitay@ozlabs.org>

--- libipl/p10/common.C
+++ libipl/p10/common.C
@@ -34,13 +34,16 @@ int ipl_istep_via_hostboot(int major, int minor)
 {
 	fapi2::ReturnCode fapi_rc;
 	struct pdbg_target *pib;
+        uint64_t retry_limit_ms = 30 * 60 * 1000;
+        uint64_t delay_ms = 100;
 	int rc = 0;
 
 	pdbg_for_each_class_target("pib", pib) {
 		if (ipl_mode() == IPL_DEFAULT && pdbg_target_index(pib) != 0)
 			continue;
 
-		fapi_rc = p10_do_fw_hb_istep(pib, major, minor);
+		fapi_rc = p10_do_fw_hb_istep(pib, major, minor,
+					     retry_limit_ms, delay_ms);
 		if (fapi_rc != fapi2::FAPI2_RC_SUCCESS) {
 			ipl_log("Istep %d.%d failed on chip %d, rc=%d\n",
 				major, minor, pdbg_target_index(pib), rc);
