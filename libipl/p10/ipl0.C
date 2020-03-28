9a205a035fe4e7dd1f0a1aa5ab5f3b86f305c711
Jayanth Othayoth <ojayanth@in.ibm.com>
2020-03-28 02:24:01 -0500

    libipl: enable sbe_config_update istep
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

--- libipl/p10/ipl0.C
+++ libipl/p10/ipl0.C
@@ -7,9 +7,11 @@ extern "C" {
 
 #include "libipl.H"
 #include "libipl_internal.H"
+
 #include <ekb/chips/p10/procedures/hwp/perv/p10_start_cbs.H>
 #include <ekb/chips/p10/procedures/hwp/perv/p10_setup_ref_clock.H>
 #include <ekb/chips/p10/procedures/hwp/perv/p10_clock_test.H>
+#include <ekb/chips/p10/procedures/hwp/perv/p10_setup_sbe_config.H>
 
 static void ipl_pre0(void)
 {
@@ -76,9 +78,66 @@ static int ipl_proc_select_boot_master(void)
 	return -1;
 }
 
+static void set_core_status(void)
+{
+	struct pdbg_target *core;
+
+	pdbg_for_each_class_target("core", core) {
+		uint8_t buf[5];
+
+		if (!pdbg_target_get_attribute_packed(core,
+						      "ATTR_HWAS_STATE",
+						      "41",
+						      buf)) {
+			ipl_log(IPL_ERROR, "Attribute [ATTR_HWAS_STATE] read failed\n");
+			continue;
+		}
+
+		// TODO hwas state should be set based on guard records status.
+		buf[4] |= 0x40;
+
+		if (!pdbg_target_set_attribute_packed(core,
+						      "ATTR_HWAS_STATE",
+						      "41",
+						      buf)) {
+			ipl_log(IPL_ERROR, "Attribute [ATTR_HWAS_STATE] update failed \n");
+			continue;
+		}
+	}
+}
+
 static int ipl_sbe_config_update(void)
 {
-	return -1;
+	struct pdbg_target *root, *proc;
+	uint32_t boot_flags = 0;
+	uint8_t istep_mode;
+
+	root = pdbg_target_root();
+	if (!pdbg_target_get_attribute(root, "ATTR_ISTEP_MODE", 1, 1, &istep_mode)) {
+		ipl_log(IPL_ERROR, "Attribute [ATTR_ISTEP_MODE] read failed \n");
+		return 1;
+	}
+
+	// Bit 0 indicates istep IPL (0b1) (Used by SBE, HB – ATTR_ISTEP_MODE)
+	if (istep_mode)
+		boot_flags = 0x80000000;
+
+	if (!pdbg_target_set_attribute(root, "ATTR_BOOT_FLAGS", 4, 1, &boot_flags)) {
+		ipl_log(IPL_ERROR, "Attribute [ATTR_BOOT_FLAGS] update failed \n");
+		return 1;
+	}
+
+	//Initialize core target functional status
+	set_core_status();
+
+	pdbg_for_each_class_target("proc", proc) {
+		if (ipl_mode() == IPL_DEFAULT && pdbg_target_index(proc) != 0)
+			continue;
+
+		ipl_error_callback((p10_setup_sbe_config(proc) == fapi2::FAPI2_RC_SUCCESS));
+	}
+
+	return 0;
 }
 
 static int ipl_sbe_start(void)
