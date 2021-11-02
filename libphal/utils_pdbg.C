0b8df53c637850b6135f86147b6f9a9496590ebf
Jayanth Othayoth <ojayanth@in.ibm.com>
2021-11-02 06:10:44 -0500

    libphal: utility function to get target from bin path
    Added utility function to return pdbg target associated to
    user provided bin path from phal device tree.
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- libphal/utils_pdbg.C
+++ libphal/utils_pdbg.C
@@ -2,6 +2,9 @@
 #include "phal_exception.H"
 #include "utils_pdbg.H"
 
+#include <cstring>
+#include <sstream>
+
 namespace openpower::phal::utils
 {
 namespace pdbg
@@ -9,6 +12,29 @@ namespace pdbg
 
 using namespace openpower::phal::logging;
 using namespace openpower::phal;
+
+/**
+ * Used to return in pdbg tarverse callback function
+ *
+ * The value for constexpr defined based on pdbg_target_traverse function usage.
+ */
+constexpr int continueTgtTraversal = 0;
+constexpr int requireTgtFound = 1;
+
+/**
+ * Used to pass buffer to pdbg callback api to get required target
+ * info based on given data (attribute).
+ */
+struct TargetInfo {
+	ATTR_PHYS_BIN_PATH_Type physBinPath;
+	struct pdbg_target *target;
+
+	TargetInfo()
+	{
+		memset(&physBinPath, 0, sizeof(physBinPath));
+		target = nullptr;
+	}
+};
 
 struct pdbg_target *getPibTarget(struct pdbg_target *proc)
 {
@@ -46,6 +72,60 @@ struct pdbg_target *getFsiTarget(struct pdbg_target *proc)
 		throw pdbgError_t(exception::PDBG_TARGET_NOT_OPERATIONAL);
 	}
 	return fsi;
+}
+
+/**
+ * @brief callback function, which are used to get pdbg target
+ * based on binary path.
+ *
+ * @param[in] target current device tree target
+ * @param[out] appPrivData used for accessing|storing from|to application
+ *
+ * @return 0 to continue traverse, non-zero to stop traverse
+ **/
+int pdbgCallbackToGetTgt(struct pdbg_target *tgt, void *appPrivData)
+{
+	TargetInfo *targetInfo = static_cast<TargetInfo *>(appPrivData);
+
+	ATTR_PHYS_BIN_PATH_Type physBinPath;
+	if (!pdbg_target_get_attribute(
+		tgt, "ATTR_PHYS_BIN_PATH",
+		std::stoi(dtAttr::fapi2::ATTR_PHYS_BIN_PATH_Spec),
+		dtAttr::fapi2::ATTR_PHYS_BIN_PATH_ElementCount, physBinPath)) {
+		return continueTgtTraversal;
+	}
+
+	if (std::memcmp(physBinPath, targetInfo->physBinPath,
+			dtAttr::fapi2::ATTR_PHYS_BIN_PATH_ElementCount) != 0) {
+		return continueTgtTraversal;
+	}
+
+	targetInfo->target = tgt;
+
+	return requireTgtFound;
+}
+
+struct pdbg_target *getTgtFromBinPath(const ATTR_PHYS_BIN_PATH_Type &binPath)
+{
+
+	TargetInfo targetInfo;
+	std::memcpy(&targetInfo.physBinPath, binPath,
+		    dtAttr::fapi2::ATTR_PHYS_BIN_PATH_ElementCount);
+
+	int ret = pdbg_target_traverse(NULL, pdbgCallbackToGetTgt, &targetInfo);
+
+	if (ret != requireTgtFound) {
+		std::stringstream ss;
+		for (uint32_t a = 0; a < sizeof(ATTR_PHYS_BIN_PATH_Type); a++) {
+			ss << " 0x" << std::hex << static_cast<int>(binPath[a]);
+		}
+		std::string s = ss.str();
+		log(level::ERROR,
+		    "Given PATH(%s) not found in phal device tree", s.c_str());
+		return nullptr;
+	}
+
+	return targetInfo.target;
 }
 
 } // namespace pdbg
